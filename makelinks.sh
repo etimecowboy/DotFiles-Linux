6f5e83404f9078842a66a5cb3bbecf15c7485ab0
Xin Yang <etimecowboy@gmail.com>
2015-02-09 02:22:53 +0000

    Add makelinks.sh for symbolic link creation

---
+++ makelinks.sh
@@ -0,0 +1,23 @@
+ln -s ~/src/DotEmacs ~/.emacs.d
+mv ~/.bashrc ~/.bashrc.orig
+ln -s ~/src/DotFiles-Linux/.bashrc ~/.bashrc
+mv ~/.bash_logout ~/.bash_logout.orig
+ln -s ~/src/DotFiles-Linux/.bash_logout ~/.bash_logout
+mv ~/.fbtermrc ~/.fbtermrc.orig
+ln -s ~/src/DotFiles-Linux/.fbtermrc ~/.fbtermrc
+mv ~/.latexmkrc ~/.latexmkrc.orig
+ln -s ~/src/DotFiles-Linux/.latexmkrc ~/.latexmkrc
+mv ~/.profile ~/.profile.orig
+ln -s ~/src/DotFiles-Linux/.profile ~/.profile
+mv ~/.Xresources ~/.Xresources.orig
+ln -s ~/src/DotFiles-Linux/.Xresources ~/.Xresources
+mv ~/.gitconfig ~/.gitconfig.orig
+ln -s ~/src/DotFiles-Linux/.gitconfig ~/.gitconfig
+mv ~/.urxvt ~/.urxvt.orig
+ln -s ~/src/DotFiles-Linux/.urxvt ~/.urxvt
+mv ~/.config/openbox/lxde-rc.xml ~/.config/openbox/lxde-rc.xml.orig
+ln -s ~/src/DotFiles-Linux/.config/openbox/debian-testing-jessie-lxde-rc.xml ~/.config/openbox/lxde-rc.xml
+ln -s ~/src/DotFiles-Linux/.local/share/applications/org-protocol.desktop ~/.local/share/applications/org-protocol.desktop
+ln -s ~/src/DotFiles-Linux/.tmux.conf ~/.tmux.conf
+mv ~/.xsession ~/.xsession.orig
+ln -s ~/src/DotFiles-Linux/.xsession ~/.xsession
