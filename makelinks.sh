90bc9349834e468486004441f2c2080352555052
Xin Yang <etimecowboy@gmail.com>
2015-03-28 00:22:27 +0000

    Change makelinks.sh script
    * makelinks.sh: clear code, enable emacs org-protocol

--- makelinks.sh
+++ makelinks.sh
@@ -1,23 +1,42 @@
-ln -s ~/src/DotEmacs ~/.emacs.d
+#!/bin/bash
+
+## X window
+mv ~/.Xresources ~/.Xresources.orig
+ln -s ~/src/DotFiles-Linux/.Xresources ~/.Xresources
+mv ~/.xsession ~/.xsession.orig
+ln -s ~/src/DotFiles-Linux/.xsession ~/.xsession
+
+## LXDE desktop (openbox WM)
+# mv ~/.config/openbox/lxde-rc.xml ~/.config/openbox/lxde-rc.xml.orig
+# ln -s ~/src/DotFiles-Linux/.config/openbox/debian-testing-jessie-lxde-rc.xml ~/.config/openbox/lxde-rc.xml
+
+## Bash
 mv ~/.bashrc ~/.bashrc.orig
 ln -s ~/src/DotFiles-Linux/.bashrc ~/.bashrc
 mv ~/.bash_logout ~/.bash_logout.orig
 ln -s ~/src/DotFiles-Linux/.bash_logout ~/.bash_logout
-mv ~/.fbtermrc ~/.fbtermrc.orig
-ln -s ~/src/DotFiles-Linux/.fbtermrc ~/.fbtermrc
-mv ~/.latexmkrc ~/.latexmkrc.orig
-ln -s ~/src/DotFiles-Linux/.latexmkrc ~/.latexmkrc
 mv ~/.profile ~/.profile.orig
 ln -s ~/src/DotFiles-Linux/.profile ~/.profile
-mv ~/.Xresources ~/.Xresources.orig
-ln -s ~/src/DotFiles-Linux/.Xresources ~/.Xresources
-mv ~/.gitconfig ~/.gitconfig.orig
-ln -s ~/src/DotFiles-Linux/.gitconfig ~/.gitconfig
+
+## fbterm
+mv ~/.fbtermrc ~/.fbtermrc.orig
+ln -s ~/src/DotFiles-Linux/.fbtermrc ~/.fbtermrc
+
+## urxvt
 mv ~/.urxvt ~/.urxvt.orig
 ln -s ~/src/DotFiles-Linux/.urxvt ~/.urxvt
-# mv ~/.config/openbox/lxde-rc.xml ~/.config/openbox/lxde-rc.xml.orig
-# ln -s ~/src/DotFiles-Linux/.config/openbox/debian-testing-jessie-lxde-rc.xml ~/.config/openbox/lxde-rc.xml
-# ln -s ~/src/DotFiles-Linux/.local/share/applications/org-protocol.desktop ~/.local/share/applications/org-protocol.desktop
+
+## tmux
 ln -s ~/src/DotFiles-Linux/.tmux.conf ~/.tmux.conf
-mv ~/.xsession ~/.xsession.orig
-ln -s ~/src/DotFiles-Linux/.xsession ~/.xsession
+
+## latexmk
+mv ~/.latexmkrc ~/.latexmkrc.orig
+ln -s ~/src/DotFiles-Linux/.latexmkrc ~/.latexmkrc
+
+## Emacs
+ln -s ~/src/DotEmacs ~/.emacs.d
+ln -s ~/src/DotFiles-Linux/.local/share/applications/org-protocol.desktop ~/.local/share/applications/org-protocol.desktop
+
+## Git
+mv ~/.gitconfig ~/.gitconfig.orig
+ln -s ~/src/DotFiles-Linux/.gitconfig ~/.gitconfig
