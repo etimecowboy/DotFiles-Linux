6021274b958d7c9f66aeda8805ba2106c45ecfa0
Xin Yang <etimecowboy@gmail.com>
2018-04-09 20:58:37 +0800

    Learn to create anaconda environment.

--- install.sh
+++ install.sh
@@ -134,26 +134,32 @@ chmod +x Anaconda2-5.1.0-Linux-x86_64.sh
 chmod +x Anaconda3-5.1.0-Linux-x86_64.sh
 
 ### Anaconda2 (installed in /opt/anaconda2)
+# Use this as the default python
 conda="/opt/anaconda2/bin/conda"
 sudo "$conda" config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
 sudo "$conda" config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
 sudo "$conda" config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
 sudo "$conda" config --set show_channel_urls yes
-sudo "$conda" install autopep8
-sudo "$conda" install -c conda-forge jupyter_contrib_nbextensions
-
-### Anaconda3 (installed in /opt/anaconda3)
-conda="/opt/anaconda3/bin/conda"
-sudo "$conda" config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
-sudo "$conda" config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
-sudo "$conda" config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
-sudo "$conda" config --set show_channel_urls yes
+sudo "$conda" update -n base condae
+sudo "$conda" update --all
 sudo "$conda" install autopep8
 sudo "$conda" install -c conda-forge jupyter_contrib_nbextensions
 # jupyterhub is only avaiable for python 3
+"$conda" -n py35 python=3.5 anaconda
+source activate py35
+
+### Anaconda3 (installed in /opt/anaconda3)
+# conda="/opt/anaconda3/bin/conda"
+# sudo "$conda" config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+# sudo "$conda" config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+# sudo "$conda" config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
+# sudo "$conda" config --set show_channel_urls yes
+# sudo "$conda" install autopep8
+# sudo "$conda" install -c conda-forge jupyter_contrib_nbextensions
+# jupyterhub is only avaiable for python 3
 sudo apt-get install npm nodejs-legacy
 sudo "$conda" install -c conda-forge jupyterhub
-sudo "$conda" install notebook
+# sudo "$conda" install notebook
 
 ## jupyter
 ## NOTE: don't use this jupyter config when using jupyterhub, otherwise you cannot start jupyterhub
