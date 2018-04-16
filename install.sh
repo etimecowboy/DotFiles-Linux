ee3a915e08b07b7e12373c0ebf05cd8df6bd0114
Xin Yang <etimecowboy@gmail.com>
2018-04-16 17:02:24 +0800

    Rewrite install.sh.

--- install.sh
+++ install.sh
@@ -1,107 +1,41 @@
 #!/bin/bash
 # Install required CLI tools before any other software
+# Inputs:
+#           - $1 : path to the my src folder (where I put my git-controlled source files)
 
 ## The first thing to do is to install ssh and get your private ssh key
 if [ ! -e ~/.ssh/id_rsa ]; then exit 1; fi
 
 ## Update system packages
-sudo apt-get update        # Fetches the list of available updates
-sudo apt-get upgrade       # Strictly upgrades the current packages
+sudo apt update        # Fetches the list of available updates
+sudo apt -y upgrade    # Strictly upgrades the current packages
 
 ## Install some CLI software
-sudo apt-get install ssh git aptitude build-essential cmake automake clang cscope scons wget
-sudo apt-get install sox cmus ranger mc htop iftop nmon tmux vim powerline w3m w3m-img
+sudo apt -y install ssh git aptitude build-essential cmake automake clang cscope scons wget
+sudo apt -y install sox cmus ranger mc htop iftop nmon tmux vim powerline w3m w3m-img trash-cli
 
 ## Install some GUI software
 # sudo apt-get install rxvt-unicode-256color gimp audacity fbterm fcitx-frontend-fbterm
 
 ## Clone my git
-dirName="src"
-myDir=~/"$dirName"
-if [ ! -d "$myDir" ]; then mkdir -p "$myDir"; fi
-git clone --depth=1 -j8 git@github.com:etimecowboy/DotFiles-Linux.git "$myDir"/
-git clone --depth=1 -j8 git@github.com:etimecowboy/DotSpacemacs.git "$myDir"/
-git clone --recurse-submodules --depth=1 -j8 git@github.com:etimecowboy/DotEmacs.git "$myDir"/
+if [ ! -d $1 ]; then mkdir -p $1; fi
+export IFS=";"
+mygits="DotFiles-Linux;DotSpacemacs" #DotEmacs
+for gitName in $mygits; do 
+    localGit=$1/"$gitName"
+    if [ ! -d "$localGit" ]; then 
+        git clone --recurse-submodules --depth=1 git@github.com:etimecowboy/"$gitName".git "$localGit"
+    fi
+    # TODO: check and handle different git status
+done
 
-## Make links
-
-### Global variables
-srcDir=pwd
-
-### X window
-fileName=".Xresources"
-myFile=~/"$fileName"
-if [ -e "$myFile" ]; then mv "$myFile" "$myFile".orig; fi
-ln -s "$srcDir""$fileName" "$myFile"
-
-# mv ~/.xsession ~/.xsession.orig
-# ln -s ~/src/DotFiles-Linux/.xsession ~/.xsession
-
-## LXDE desktop (openbox WM)
-# mv ~/.config/openbox/lxde-rc.xml ~/.config/openbox/lxde-rc.xml.orig
-# ln -s ~/src/DotFiles-Linux/.config/openbox/debian-testing-jessie-lxde-rc.xml ~/.config/openbox/lxde-rc.xml
-
-### Bash
-fileName=".bashrc"
-myFile=~/"$fileName"
-if [ -e "$myFile" ]; then mv "$myFile" "$myFile".orig; fi
-ln -s "$srcDir""$fileName" "$myFile"
-
-fileName=".bash_logout"
-myFile=~/"$fileName"
-if [ -e "$myFile" ]; then mv "$myFile" "$myFile".orig; fi
-ln -s "$srcDir""$fileName" "$myFile"
-
-fileName=".profile"
-myFile=~/"$fileName"
-if [ -e "$myFile" ]; then mv "$myFile" "$myFile".orig; fi
-ln -s "$srcDir""$fileName" "$myFile"
-
-### Git
-fileName=".gitconfig"
-myFile=~/"$fileName"
-if [ -e "$myFile" ]; then mv "$myFile" "$myFile".orig; fi
-ln -s "$srcDir""$fileName" "$myFile"
-
-### fbterm
-fileName=".fbtermrc"
-myFile=~/"$fileName"
-if [ -e "$myFile" ]; then trash "$myFile"; fi
-ln -s "$srcDir""$fileName" "$myFile"
-
-### urxvt
-dirName=".urxvt"
-myDir=~/"$dirName"
-if [ -d "$myDir" ]; then trash "$myDir"; fi
-ln -s "$srcDir""$dirName" "$myDir"
-
-### tmux
-fileName=".tmux.conf"
-myFile=~/"$fileName"
-if [ -e "$myFile" ]; then mv "$myFile" "$myFile".orig; fi
-ln -s "$srcDir""$fileName" "$myFile"
-
-# ### tmuxinator
-# dirName=".tmuxinator"
-# myDir=~/"$dirName"
-# if [ -d "$myDir" ]; then trash "$myDir"; fi
-# ln -s "$srcDir""$dirName" "$myDir"
-
-### latexmk
-fileName=".latexmkrc"
-myFile=~/"$fileName"
-if [ -e "$myFile" ]; then mv "$myFile" "$myFile".orig; fi
-ln -s "$srcDir""$fileName" "$myFile"
-
-### ranger
-## FIXME: error
-dirName=".config/ranger"
-myDir=~/"$dirName"
-if [ -d "$myDir" ]; then trash "$myDir"; fi
-ln -s "$srcDir""$dirName" "$myDir"
-
-### mc
-dirName=".config/mc"
-myDir=~/"$dirName"
-if [ -d "$myDir" ]; then mv "$myDir" "$myDir".orig; fi
-ln -s "$srcDir""$dirName" "$myDir"
+## make links
+export IFS=";"
+dotfiles=".Xresources;.bashrc;.bash_logout;.profile;.gitconfig;.fbtermrc;.tmux.conf;.latexmkrc;.urxvt;.config/ranger;.config/mc" #.xsession;.config/openbox/lxde-rc.xml
+for fileName in $dotfiles; do
+    myFile=~/"$fileName"
+    # echo "$myFile is created..."
+    if [[ -L "$myFile" ]] || [[ -d "$myFile" ]]; then trash "$myFile"; fi
+    if [ -e "$myFile" ]; then mv "$myFile" "$myFile".orig; fi
+    ln -s $1/DotFiles-Linux/"$fileName" "$myFile"
+done
