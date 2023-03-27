d7e885eb48b3a2d5edebe9a64aaaeff195c50e82
Xin <etimecowboy@gmail.com>
2023-03-27 09:04:24 +0800

    Add wezterm config.

---
+++ .config/wezterm/extra/ssh.lua
@@ -0,0 +1,12 @@
+return {
+   {
+      -- This name identifies the domain
+      name = 'xin@zbox',
+      -- The hostname or address to connect to. Will be used to match settings
+      -- from your ssh config file
+      remote_address = '192.168.0.23',
+      -- The username to use on the remote host
+      username = 'xin',
+      multiplexing = 'None',
+  },
+}
